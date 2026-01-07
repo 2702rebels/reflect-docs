96af6096a75d4c9b5c63c0d3a1622679884f74b6
Oleg Grodzevich <oleg@oneiq.com>
2026-01-07 18:24:14 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Reflect Documentation
 
-This repository contains documentation for [Reflect](https://github.com/2702rebels/reflect) and is available online at [reflect-docs.2702rebels.com](https://github.com/2702rebels/reflect).
+This repository contains documentation for [Reflect](https://github.com/2702rebels/reflect) and is available online at
+[reflect-docs.2702rebels.com](https://github.com/2702rebels/reflect).
---
+++ astro.config.mjs
@@ -0,0 +1,46 @@
+// @ts-check
+import starlight from "@astrojs/starlight";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, envField } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  trailingSlash: "never",
+  integrations: [
+    starlight({
+      title: "Reflect Documentation",
+      logo: {
+        alt: "Reflect",
+        src: "./public/reflect.svg",
+      },
+      social: [{ icon: "github", label: "GitHub", href: "https://github.com/2702rebels/realm/tree/main/apps/reflect" }],
+      sidebar: [
+        {
+          label: "Overview",
+          slug: "overview",
+        },
+        {
+          label: "Getting Started",
+          autogenerate: { directory: "getting-started" },
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+        {
+          label: "Contribute to Reflect",
+          slug: "contribute",
+        },
+      ],
+      customCss: ["./src/styles/global.css"],
+    }),
+  ],
+  env: {
+    schema: {
+      RELEASE_VERSION: envField.string({ context: "client", access: "public" }),
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
